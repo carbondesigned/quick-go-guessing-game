0f4648cdcf73876b6075f1b476aa16d5761f9d49
Dylan <dylan.reeder5@gmail.com>
2022-05-25 19:38:22 -0400

    fix: produces random int each program run

--- main.go
+++ main.go
@@ -3,16 +3,19 @@ package main
 import (
 	"fmt"
 	"math/rand"
+	"time"
 )
 
 func main() {
+	// produces a random number
+	rand.Seed(time.Now().UnixNano())
+
 	var MAX_NUMBER int = 100
 	var MIN_NUMBER int = 1
 	var guess int
 
 	// generate a new random number between 1 and 100 each time the program runs
-	var answer = rand.Intn(MAX_NUMBER-MIN_NUMBER+1) + MIN_NUMBER
-	println(answer)
+	var answer = rand.Intn(MAX_NUMBER - MIN_NUMBER)
 	println("Guessing Game")
 
 	fmt.Printf("Enter a number between %v and %v: ", MIN_NUMBER, MAX_NUMBER)
@@ -22,6 +25,9 @@ func main() {
 	fmt.Println("You got it!")
 }
 
+// "If the guess is not equal to the answer, print a message and ask for another guess."
+//
+// The function takes two arguments: guess and answer. It returns a boolean value
 func calculateGuess(guess int, answer int) bool {
 	for guess != answer {
 		if guess > answer {
